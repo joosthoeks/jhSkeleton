8b0c91ec91a9c997c5988c766f0b48583f26a952
Joost Hoeks <joosthoeks@gmail.com>
2014-08-29 19:27:51 +0000

    added if

--- app/controllers/ajaxController.php
+++ app/controllers/ajaxController.php
@@ -1,4 +1,11 @@
 <?php
-$ajaxData = 'ajaxData';
+if (!isset($_POST['type'])
+        || !isset($_POST['key'])
+        || !isset($_POST['value'])
+        ) {
+    exit();
+}
+
+$ajaxData = '';
 
 require $appPath.'views/page/ajaxView.php';
