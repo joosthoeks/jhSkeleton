ba0d54be6df90b1a364eb4d3a1f94cd0ecf7ffe9
joosthoeks <joosthoeks@gmail.com>
2014-10-21 21:47:39 +0000

    modified path to jquery js

--- app/views/template/layout.inc.php
+++ app/views/template/layout.inc.php
@@ -52,7 +52,7 @@ foreach ($requireViewArr as $view) {
     
     </div>
     
-<script type="text/javascript" src="https://code.jquery.com/jquery.min.js"></script>
+<script type="text/javascript" src="./js/jquery.min.js"></script>
 <script type="text/javascript" src="./js/bootstrap.min.js"></script>
 <script type="text/javascript" src="./js/functions.js"></script>
 <script type="text/javascript" src="./js/script.js"></script>
