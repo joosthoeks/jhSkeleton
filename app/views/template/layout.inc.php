d1a705660ec18bb5a5725a8a3e66d9a593217679
joosthoeks <joosthoeks@gmail.com>
2014-10-21 21:44:36 +0000

    modified path to bootstrap js

--- app/views/template/layout.inc.php
+++ app/views/template/layout.inc.php
@@ -53,7 +53,7 @@ foreach ($requireViewArr as $view) {
     </div>
     
 <script type="text/javascript" src="https://code.jquery.com/jquery.min.js"></script>
-<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="./js/bootstrap.min.js"></script>
 <script type="text/javascript" src="./js/functions.js"></script>
 <script type="text/javascript" src="./js/script.js"></script>
 
