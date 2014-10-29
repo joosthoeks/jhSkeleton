e5714960e8153f0444df3d49c576774d88638b57
joosthoeks <joosthoeks@gmail.com>
2014-10-29 20:38:22 +0000

    added grid

--- app/views/page/aboutView.php
+++ app/views/page/aboutView.php
@@ -1,2 +1,6 @@
-<h2><?php echo $pageTitle; ?></h2>
-<p><?php echo $pageDescription; ?></p>
+<div class="row">
+    <div class="col-md-12">
+        <h2><?php echo $pageTitle; ?></h2>
+        <p><?php echo $pageDescription; ?></p>
+    </div>
+</div>
