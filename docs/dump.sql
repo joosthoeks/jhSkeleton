c10c60051b79587a1e1586f1007bcd93f46dd64f
joosthoeks <joosthoeks@gmail.com>
2014-11-09 20:41:22 +0000

    modified dump sql

--- docs/dump.sql
+++ docs/dump.sql
@@ -8,21 +8,17 @@ SET time_zone = "+00:00";
 
 
 CREATE TABLE IF NOT EXISTS `page` (
-`id` bigint(20) unsigned NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `key` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `keywords` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `div` text COLLATE utf8_unicode_ci NOT NULL
+  `div` text COLLATE utf8_unicode_ci NOT NULL,
+  `lastUpdated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
-
-ALTER TABLE `page`
- ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `page`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
