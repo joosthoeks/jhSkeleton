54aba45a426b594d225631f118d0f5c96561ddb9
joosthoeks <joosthoeks@gmail.com>
2014-11-19 00:28:52 +0000

    added function createTable

--- app/models/pageModel.php
+++ app/models/pageModel.php
@@ -66,4 +66,24 @@ class pageModel
         $sql = "DELETE FROM $this->table WHERE id = $id";
         $this->db->exec($sql);
     }
+    
+    public function createTable()
+    {
+        $sql = "".
+                'SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";'.
+                'SET time_zone = "+00:00";'.
+                "CREATE TABLE IF NOT EXISTS `page` (".
+                "`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,".
+                "`key` varchar(255) COLLATE utf8_unicode_ci NOT NULL,".
+                "`title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,".
+                "`description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,".
+                "`keywords` varchar(255) COLLATE utf8_unicode_ci NOT NULL,".
+                "`div` text COLLATE utf8_unicode_ci NOT NULL,".
+                "`lastUpdated` datetime NOT NULL,".
+                "`created` datetime NOT NULL,".
+                "PRIMARY KEY (`id`)".
+                ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;"
+                ;
+        $this->db->exec($sql);
+    }
 }
