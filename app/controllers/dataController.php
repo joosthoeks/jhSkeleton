053d8958872c1e6a0326b84b8ccfce7e76f5f3d2
joosthoeks <joosthoeks@gmail.com>
2014-11-29 18:31:05 +0000

    added data controller and view

---
+++ app/controllers/dataController.php
@@ -0,0 +1,11 @@
+<?php
+if (!isset($_GET['type'])
+        || !isset($_GET['key'])
+        || !isset($_GET['value'])
+        ) {
+    exit();
+}
+
+$dataOutput = '';
+
+require $appPath.'views/page/dataView.php';
