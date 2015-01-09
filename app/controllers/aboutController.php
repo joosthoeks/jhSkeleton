8afd2f29d885b4cef34b444b1c2236634462892e
joosthoeks <joosthoeks@gmail.com>
2015-01-09 20:13:09 +0000

    added aboutController class

--- app/controllers/aboutController.php
+++ app/controllers/aboutController.php
@@ -1,23 +1,71 @@
 <?php
-$pageTitle = 'About';
-$pageDescription = '';
-$pageKeywords = '';
+class aboutController
+{
+    private $page;
+    private $pageTitle = 'About';
+    private $pageDescription = '';
+    private $pageKeywords = '';
+    
+    // add page specific css:
+    private $cssArr = array(
+    //    'css/example',
+    );
 
-// add page specific css:
-$requireCssArr = array(
-//    'css/example',
-);
+    // add page to layout:
+    private $viewArr = array(
+    //    'div/navigation',
+        'page/about',
+    );
 
-// add page to layout:
-$requireViewArr = array(
-//    'div/navigation',
-    'page/'.$page,
-);
+    // add page specific js:
+    private $jsArr = array(
+    //    'js/example',
+    );
+    
+    public function __construct($page)
+    {
+        $this->page = $page;
+    }
+    
+    public function getPage()
+    {
+        return $this->page;
+    }
 
-// add page specific js:
-$requireJsArr = array(
-//    'js/example',
-);
-
-// get layout:
-require 'layout.inc.php';
+    public function getPageTitle()
+    {
+        return $this->pageTitle;
+    }
+    
+    public function getPageDescription()
+    {
+        return $this->pageDescription;
+    }
+    
+    public function getPageKeywords()
+    {
+        return $this->pageKeywords;
+    }
+    
+    public function getCssArr()
+    {
+        return $this->cssArr;
+    }
+    
+    public function getViewArr()
+    {
+        return $this->viewArr;
+    }
+    
+    public function getJsArr()
+    {
+        return $this->jsArr;
+    }
+    
+    public function __destruct()
+    {
+        $settings = new Settings();
+        // get layout:
+        require 'layout.inc.php';
+    }
+}
